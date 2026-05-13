dafea623d6aec7869a6bd4ffdef5443594f1bb2c
Raillyn-FA <rayhannxzl@gmail.com>
2026-05-13 21:20:26 +0700

    Add files via upload

---
+++ soal_3/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /logs
+touch /logs/libraryit.log
+
+chmod -R 775 /libraryit/ebooks
+chmod -R 775 /libraryit/papers
+
+chmod -R 750 /libraryit/sourcecode
+
+chmod -R 555 /libraryit/docs
+
+chown -R root:staff /libraryit
+
+tail -f /logs/libraryit.log &
+smbd --foreground --no-process-group
